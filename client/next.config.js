fa7d64d9f4cc21afdf076867d055108ac3c0d017
Aryan Rajesh <aryanrajeshgadam.3012@gmail.com>
2026-03-14 17:59:16 +0530

    Add Content-Security-Policy header for A+ security grade

--- client/next.config.js
+++ client/next.config.js
@@ -39,6 +39,23 @@ const nextConfig = {
                         key: 'Strict-Transport-Security',
                         value: 'max-age=63072000; includeSubDomains; preload',
                     },
+                    {
+                        key: 'Content-Security-Policy',
+                        value: [
+                            "default-src 'self'",
+                            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com https://apis.google.com",
+                            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+                            "font-src 'self' https://fonts.gstatic.com",
+                            "img-src 'self' data: blob: https://res.cloudinary.com https://lh3.googleusercontent.com https://avatars.githubusercontent.com",
+                            "media-src 'self' blob: https://res.cloudinary.com https://*.onrender.com",
+                            "connect-src 'self' https://*.onrender.com wss://*.onrender.com https://accounts.google.com https://api.github.com https://res.cloudinary.com https://api.cloudinary.com",
+                            "frame-src 'self' https://accounts.google.com https://github.com",
+                            "worker-src 'self' blob:",
+                            "object-src 'none'",
+                            "base-uri 'self'",
+                            "form-action 'self' https://accounts.google.com https://github.com",
+                        ].join('; '),
+                    },
                 ],
             },
         ];
